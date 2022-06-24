45c26a8d5cdb89509fa9dbb7b054405af53b9624
Peter Mikhael <pgmikhael@gmail.com>
2022-06-24 04:15:19 -0400

    dbs

--- scripts/data/bigg_scraper.py
+++ scripts/data/bigg_scraper.py
@@ -21,13 +21,60 @@ HMDB_METABOLITES = json.load(
 )
 
 
+def get_from_metanetx(db_meta) -> dict:
+    metanetx = [f for f in db_meta.values[0].split(";") if "MetaNetX" in f]
+
+    if len(metanetx):
+        metanetid = os.path.basename(metanetx[0].split(":")[-1])
+        row = METANETX_METABOLITES[METANETX_METABOLITES["#ID"] == metanetid]
+        if len(row) > 0:
+            return {
+                "metanetx_id": metanetid,
+                "metanetx_name": row["name"].item(),
+                "metanetx_mass": row["mass"].item(),
+                "InChI": row["InChI"].item(),
+                "InChIKey": row["InChIKey"].item(),
+                "smiles": row["SMILES"].item(),
+            }
+    return dict()
+
+
+def get_hmdb(db_meta, meta_dict) -> dict:
+    hmdb = [f for f in db_meta.values[0].split(";") if "hmdb" in f]
+    if len(hmdb):
+        hmdbid = os.path.basename(hmdb[0].split(":")[-1])
+        hmdbid = hmdbid[:4] + (11 - len(hmdbid)) * "0" + hmdbid[4:]
+        row = HMDB_METABOLITES.get(hmdbid, False)
+        if row:
+            return {
+                "hmdb_id": hmdbid,
+                "InChI": row["inchi"]
+                if meta_dict.get("InChI", None) is None
+                else meta_dict["InChI"],
+                "InChIKey": row["inchikey"]
+                if meta_dict.get("InChIKey", None) is None
+                else meta_dict["InChIKey"],
+                "smiles": row["smiles"]
+                if meta_dict.get("smiles", None) is None
+                else meta_dict["smiles"],
+            }
+    return dict()
+
+
 def link_metabolite_to_db(metabolite: Metabolite) -> dict:
     """
     Link metabolite to external database
     In order, try:
         1. MetaNetX
         2. HMDB
-        3. ChEBI
+        3. BioCyc
+        4. ChEBI
+        5. Kegg
+        6. LipidMaps
+        7. InchiKey
+        8. Reactome
+        9. SEED
+        10. Unknown (Search ChEBI)
 
     Args:
         metabolite (cobra.Metabolite): metabolite object
@@ -38,52 +85,18 @@ def link_metabolite_to_db(metabolite: Metabolite) -> dict:
     Returns:
         dict: metadata for metabolite
     """
-    # Link to smiles and inchi-key
-    meta = BIGG_METABOLITES[BIGG_METABOLITES["bigg_id"] == metabolite.id][
+    db_meta = BIGG_METABOLITES[BIGG_METABOLITES["bigg_id"] == metabolite.id][
         "database_links"
     ]
-    metanetx = [f for f in meta.values[0].split(";") if "MetaNetX" in f]
-    # Try to link to MetaNetX
-    meta_dict = {}
-    if len(metanetx):
-        metanetid = os.path.basename(metanetx[0].split(":")[-1])
-        row = METANETX_METABOLITES[METANETX_METABOLITES["#ID"] == metanetid]
-        if len(row) > 0:
-            meta_dict.update(
-                {
-                    "metanetx_id": metanetid,
-                    "metanetx_name": row["name"].item(),
-                    "metanetx_mass": row["mass"].item(),
-                    "InChI": row["InChI"].item(),
-                    "InChIKey": row["InChIKey"].item(),
-                    "smiles": row["SMILES"].item(),
-                }
-            )
 
-    # HMDB
-    hmdb = [f for f in meta.values[0].split(";") if "hmdb" in f]
-    if len(hmdb):
-        hmdbid = os.path.basename(hmdb[0].split(":")[-1])
-        hmdbid = hmdbid[:4] + (11 - len(hmdbid)) * "0" + hmdbid[4:]
-        row = HMDB_METABOLITES.get(hmdbid, False)
-        if row:
-            meta_dict.update(
-                {
-                    "hmdb_id": hmdbid,
-                    "InChI": row["inchi"]
-                    if meta_dict.get("InChI", None) is None
-                    else meta_dict["InChI"],
-                    "InChIKey": row["inchikey"]
-                    if meta_dict.get("InChIKey", None) is None
-                    else meta_dict["InChIKey"],
-                    "smiles": row["smiles"]
-                    if meta_dict.get("smiles", None) is None
-                    else meta_dict["smiles"],
-                }
-            )
+    # Try MetaNetX
+    meta_dict = get_from_metanetx(db_meta)
+
+    # Try HMDB
+    meta_dict.update(get_hmdb(db_meta, meta_dict))
 
     # Try to link to ChEBI
-    elif not meta_dict.get("smiles", False):
+    if meta_dict.get("smiles", False):
         ch = ChEBI()
         try:
             name_search = ch.getLiteEntity(metabolite.name, maximumResults=5000)
@@ -93,7 +106,7 @@ def link_metabolite_to_db(metabolite: Metabolite) -> dict:
 
         try:
             formula_search = ch.getLiteEntity(
-                metabolite.formula, searchCategory="FORMULA",  maximumResults=5000
+                metabolite.formula, searchCategory="FORMULA", maximumResults=5000
             )
             formula_search_ids = [str(r.chebiId) for r in formula_search]
         except:
@@ -108,10 +121,10 @@ def link_metabolite_to_db(metabolite: Metabolite) -> dict:
             matched_ids = [m for m in name_search_ids if m in formula_search_ids]
         else:
             matched_ids = name_search_ids + formula_search_ids
-        
+
         all_complete_entities = []
         for j in range(0, len(matched_ids), 50):
-            complete_entities = ch.getCompleteEntityByList(matched_ids[j:j+50])
+            complete_entities = ch.getCompleteEntityByList(matched_ids[j : j + 50])
             all_complete_entities.extend(complete_entities)
 
         exact_matches = [
@@ -120,14 +133,21 @@ def link_metabolite_to_db(metabolite: Metabolite) -> dict:
             if met["Formulae"][0].data == metabolite.formula
             or met["chebiAsciiName"].lower() == metabolite.name.lower()
         ]
-        
+
         # if no matches, then find the closest one
         if len(exact_matches) == 0:
-            closest_mass = sorted(all_complete_entities, key=lambda x: abs(metabolite.formula_weight - float(x["mass"])))[0]
+            closest_mass = sorted(
+                all_complete_entities,
+                key=lambda x: abs(metabolite.formula_weight - float(x["mass"])),
+            )[0]
             exact_matches = [all_complete_entities.index(closest_mass)]
             meta_dict.setdefault("errors", [])
-            meta_dict['errors'].append("exact metabolite not found, metabolite with closes mass used")
-            warnings.warn(f"exact metabolite not found, metabolite with closes mass used for metabolite {metabolite.id}")
+            meta_dict["errors"].append(
+                "exact metabolite not found, metabolite with closes mass used"
+            )
+            warnings.warn(
+                f"exact metabolite not found, metabolite with closes mass used for metabolite {metabolite.id}"
+            )
 
         match = dict(all_complete_entities[exact_matches[0]])
 
