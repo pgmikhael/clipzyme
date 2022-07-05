b5d328a69beecf7067dad61506bf9ffafcaff2b0
Itamar Chinn <itamarc@mit.edu>
2022-07-05 14:06:16 -0400

    implemented parts of dataset

--- nox/datasets/indrellinkpred.py
+++ nox/datasets/indrellinkpred.py
@@ -12,7 +12,7 @@ from nox.utils.registry import register_object, md5
 from nox.utils.classes import Nox, set_nox_type
 from nox.datasets.utils import METAFILE_NOTFOUND_ERR, LOAD_FAIL_MSG
 from nox.datasets import AbstractDataset
-from torch_geometric.data import HeteroData
+from torch_geometric.data import HeteroData, InMemoryDataset, Data, download_url
 from collections.abc import Sequence
 from torch_geometric.data.makedirs import makedirs
 import tqdm
@@ -20,7 +20,22 @@ import re
 
 
 @register_object("ind_rel_link_pred", "dataset")
-class IndRelLinkPredDataset(AbstractDataset):
+class IndRelLinkPredDataset(InMemoryDataset, AbstractDataset):
+    urls = {
+        "FB15k-237": [
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s_ind/train.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s_ind/test.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s/train.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s/valid.txt",
+        ],
+        "WN18RR": [
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s_ind/train.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s_ind/test.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s/train.txt",
+            "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s/valid.txt",
+        ],
+    }
+
     def __init__(self, args: argparse.ArgumentParser, split_group: str) -> None:
         """
         FB15k-237 or WN18RR dataset
@@ -33,21 +48,6 @@ class IndRelLinkPredDataset(AbstractDataset):
 
         super(IndRelLinkPredDataset, self).__init__()
 
-        self.urls = {
-            "FB15k-237": [
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s_ind/train.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s_ind/test.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s/train.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/fb237_%s/valid.txt",
-            ],
-            "WN18RR": [
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s_ind/train.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s_ind/test.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s/train.txt",
-                "https://raw.githubusercontent.com/kkteru/grail/master/data/WN18RR_%s/valid.txt",
-            ],
-        }
-
         self.split_group = split_group
         self.args = args
 
@@ -55,21 +55,12 @@ class IndRelLinkPredDataset(AbstractDataset):
         self.version = args.dataset_version
         assert self.name in ["FB15k-237", "WN18RR"]
         assert self.version in ["v1", "v2", "v3", "v4"]
-        # self.root = root
-        # self.transform = transform
-        # self.pre_transform = pre_transform
-        # self.pre_filter = pre_filter
-        # self._indices: Optional[Sequence] = None
-
-        # self.data = None
-        # self.slices = None
-        # self._data_list: Optional[List[Data]] = None
 
         self.init_class(args, split_group)
 
         # self.input_loader = get_sample_loader(split_group, args)
 
-        # self.dataset = self.create_dataset(split_group)
+        self.dataset = self.create_dataset(split_group)
         if len(self.dataset) == 0:
             return
 
@@ -86,54 +77,6 @@ class IndRelLinkPredDataset(AbstractDataset):
             split_group (str)
         """
         self.load_dataset(args)
-
-    @property
-    def processed_paths(self) -> List[str]:
-        r"""The absolute filepaths that must be present in order to skip
-        processing."""
-        files = to_list(self.processed_file_names)
-        return [os.path.join(self.processed_dir, f) for f in files]
-
-    def _download(self):
-        if files_exist(self.raw_paths):  # pragma: no cover
-            return
-
-        makedirs(self.raw_dir)
-        self.download()
-
-    def _process(self):
-        f = os.path.join(self.processed_dir, "pre_transform.pt")
-        if os.path.exists(f) and torch.load(f) != _repr(self.pre_transform):
-            warnings.warn(
-                f"The `pre_transform` argument differs from the one used in "
-                f"the pre-processed version of this dataset. If you want to "
-                f"make use of another pre-processing technique, make sure to "
-                f"delete '{self.processed_dir}' first"
-            )
-
-        f = os.path.join(self.processed_dir, "pre_filter.pt")
-        if os.path.exists(f) and torch.load(f) != _repr(self.pre_filter):
-            warnings.warn(
-                "The `pre_filter` argument differs from the one used in "
-                "the pre-processed version of this dataset. If you want to "
-                "make use of another pre-fitering technique, make sure to "
-                "delete '{self.processed_dir}' first"
-            )
-
-        if files_exist(self.processed_paths):  # pragma: no cover
-            return
-
-        print("Processing...", file=sys.stderr)
-
-        makedirs(self.processed_dir)
-        self.process()
-
-        path = os.path.join(self.processed_dir, "pre_transform.pt")
-        torch.save(_repr(self.pre_transform), path)
-        path = os.path.join(self.processed_dir, "pre_filter.pt")
-        torch.save(_repr(self.pre_filter), path)
-
-        print("Done!", file=sys.stderr)
 
     @property
     def num_relations(self):
@@ -261,93 +204,6 @@ class IndRelLinkPredDataset(AbstractDataset):
     def __repr__(self):
         return "%s()" % self.name
 
-    @property
-    def num_classes(self) -> int:
-        r"""Returns the number of classes in the dataset."""
-        y = self.data.y
-        if y is None:
-            return 0
-        elif y.numel() == y.size(0) and not torch.is_floating_point(y):
-            return int(self.data.y.max()) + 1
-        elif y.numel() == y.size(0) and torch.is_floating_point(y):
-            return torch.unique(y).numel()
-        else:
-            return self.data.y.size(-1)
-
-    def len(self) -> int:
-        if self.slices is None:
-            return 1
-        for _, value in nested_iter(self.slices):
-            return len(value) - 1
-        return 0
-
-    def get(self, idx: int) -> Data:
-        if self.len() == 1:
-            return copy.copy(self.data)
-
-        if not hasattr(self, "_data_list") or self._data_list is None:
-            self._data_list = self.len() * [None]
-        elif self._data_list[idx] is not None:
-            return copy.copy(self._data_list[idx])
-
-        data = separate(
-            cls=self.data.__class__,
-            batch=self.data,
-            idx=idx,
-            slice_dict=self.slices,
-            decrement=False,
-        )
-
-        self._data_list[idx] = copy.copy(data)
-
-        return data
-
-    @staticmethod
-    def collate(data_list: List[Data]) -> Tuple[Data, Optional[Dict[str, Tensor]]]:
-        r"""Collates a Python list of :obj:`torch_geometric.data.Data` objects
-        to the internal storage format of
-        :class:`~torch_geometric.data.InMemoryDataset`."""
-        if len(data_list) == 1:
-            return data_list[0], None
-
-        data, slices, _ = collate(
-            data_list[0].__class__,
-            data_list=data_list,
-            increment=False,
-            add_batch=False,
-        )
-
-        return data, slices
-
-    def copy(self, idx: Optional[IndexType] = None) -> "InMemoryDataset":
-        r"""Performs a deep-copy of the dataset. If :obj:`idx` is not given,
-        will clone the full dataset. Otherwise, will only clone a subset of the
-        dataset from indices :obj:`idx`.
-        Indices can be slices, lists, tuples, and a :obj:`torch.Tensor` or
-        :obj:`np.ndarray` of type long or bool.
-        """
-        if idx is None:
-            data_list = [self.get(i) for i in self.indices()]
-        else:
-            data_list = [self.get(i) for i in self.index_select(idx).indices()]
-
-        dataset = copy.copy(self)
-        dataset._indices = None
-        dataset._data_list = None
-        dataset.data, dataset.slices = self.collate(data_list)
-        return dataset
-
-    def nested_iter(node: Union[Mapping, Sequence]) -> Iterable:
-        if isinstance(node, Mapping):
-            for key, value in node.items():
-                for inner_key, inner_value in nested_iter(value):
-                    yield inner_key, inner_value
-        elif isinstance(node, Sequence):
-            for i, inner_value in enumerate(node):
-                yield i, inner_value
-        else:
-            yield None, node
-
     def load_dataset(self, args: argparse.ArgumentParser) -> None:
         """Loads dataset file
 
@@ -358,24 +214,111 @@ class IndRelLinkPredDataset(AbstractDataset):
             Exception: Unable to load
         """
         try:
-            if self.download.__qualname__.split(".")[0] != "Dataset":
-                self._download()
-
-            if self.process.__qualname__.split(".")[0] != "Dataset":
-                self._process()
-
-            self.dataset, self.slices = torch.load(self.processed_paths[0])
+            self.data, self.slices = torch.load(self.processed_paths[0])
 
         except Exception as e:
-            raise Exception(METAFILE_NOTFOUND_ERR.format(args.dataset_file_path, e))
+            raise Exception("Can't load dataset", e)
 
     def create_dataset(
         self, split_group: Literal["train", "dev", "test"]
     ) -> List[dict]:
         """
-        Creates the dataset of samples from json metadata file.
+        Creates the dataset
         """
-        pass
+        dataset = []
+        sample = {}
+        if self.args.dataset_variant == "FB15k-237":
+            data = self.data.data
+            self.train_data = Data(
+                edge_index=data.edge_index,
+                edge_type=data.edge_type,
+                num_nodes=data.num_nodes,
+                target_edge_index=data.train_edge_index,
+                target_edge_type=data.train_edge_type,
+            )
+            self.valid_data = Data(
+                edge_index=data.edge_index,
+                edge_type=data.edge_type,
+                num_nodes=data.num_nodes,
+                target_edge_index=data.valid_edge_index,
+                target_edge_type=data.valid_edge_type,
+            )
+            self.test_data = Data(
+                edge_index=data.edge_index,
+                edge_type=data.edge_type,
+                num_nodes=data.num_nodes,
+                target_edge_index=data.test_edge_index,
+                target_edge_type=data.test_edge_type,
+            )
+
+            if self.split_group == "train":
+                sample["graph"] = self.train_data
+            elif self.split_group == "dev":
+                sample["graph"] = self.valid_data
+            elif self.split_group == "test":
+                sample["graph"] = self.test_data
+            else:
+                raise ValueError(f"Invalid split group: {self.split_group}")
+            # self.dataset.data, self.dataset.slices = self.dataset.collate(
+            #    [train_data, valid_data, test_data]
+            # )
+        elif self.args.dataset_variant == "WN18RR":
+            # dataset = WordNet18RR(**cfg.dataset)
+            # convert wn18rr into the same format as fb15k-237
+            data = self.data.data
+            num_nodes = int(data.edge_index.max()) + 1
+            num_relations = int(data.edge_type.max()) + 1
+            edge_index = data.edge_index[:, data.train_mask]
+            edge_type = data.edge_type[data.train_mask]
+            edge_index = torch.cat([edge_index, edge_index.flip(0)], dim=-1)
+            edge_type = torch.cat([edge_type, edge_type + num_relations])
+            self.train_data = Data(
+                edge_index=edge_index,
+                edge_type=edge_type,
+                num_nodes=num_nodes,
+                target_edge_index=data.edge_index[:, data.train_mask],
+                target_edge_type=data.edge_type[data.train_mask],
+            )
+            self.valid_data = Data(
+                edge_index=edge_index,
+                edge_type=edge_type,
+                num_nodes=num_nodes,
+                target_edge_index=data.edge_index[:, data.val_mask],
+                target_edge_type=data.edge_type[data.val_mask],
+            )
+            self.test_data = Data(
+                edge_index=edge_index,
+                edge_type=edge_type,
+                num_nodes=num_nodes,
+                target_edge_index=data.edge_index[:, data.test_mask],
+                target_edge_type=data.edge_type[data.test_mask],
+            )
+            if self.split_group == "train":
+                sample["graph"] = self.train_data
+            elif self.split_group == "dev":
+                sample["graph"] = self.valid_data
+            elif self.split_group == "test":
+                sample["graph"] = self.test_data
+            else:
+                raise ValueError(f"Invalid split group: {self.split_group}")
+
+        else:
+            raise ValueError("Unknown dataset `%s`" % self.args.dataset_variant)
+
+        self.train_data.num_relations = num_relations * 2
+        self.test_data.num_relations = num_relations * 2
+        self.valid_data.num_relations = num_relations * 2
+
+        triplets = torch.cat(
+            [
+                sample["graph"].target_edge_index,
+                sample["graph"].target_edge_type.unsqueeze(0),
+            ]
+        ).t()
+
+        sample["triplets"] = triplets
+        dataset.append(sample)
+        return dataset
 
     def skip_sample(self, sample) -> bool:
         """
@@ -388,7 +331,9 @@ class IndRelLinkPredDataset(AbstractDataset):
         """
         Prints summary statement with dataset stats
         """
-        summary = f"Contructed {self.args.dataset_variant} dataset with length of {len(self.dataset)}"
+        # self.data is graph
+        # self.dataset is number of graphs
+        summary = f"Contructed {self.split_group} of {self.args.dataset_variant} dataset with length of {len(self.data)}"
         return summary
 
     def print_summary_statement(self, dataset, split_group):
@@ -408,88 +353,8 @@ class IndRelLinkPredDataset(AbstractDataset):
             sample (dict): a sample
         """
         item = {}
-        cls = self.args.dataset_variant
-        if cls == "FB15k-237":
-            data = self.dataset.data
-            if self.split_group == "train":
-                train_data = Data(
-                    edge_index=data.edge_index,
-                    edge_type=data.edge_type,
-                    num_nodes=data.num_nodes,
-                    target_edge_index=data.train_edge_index,
-                    target_edge_type=data.train_edge_type,
-                )
-                item["graph"] = train_data
-            elif self.split_group == "dev":
-                valid_data = Data(
-                    edge_index=data.edge_index,
-                    edge_type=data.edge_type,
-                    num_nodes=data.num_nodes,
-                    target_edge_index=data.valid_edge_index,
-                    target_edge_type=data.valid_edge_type,
-                )
-                item["graph"] = valid_data
-            elif self.split_group == "test":
-                test_data = Data(
-                    edge_index=data.edge_index,
-                    edge_type=data.edge_type,
-                    num_nodes=data.num_nodes,
-                    target_edge_index=data.test_edge_index,
-                    target_edge_type=data.test_edge_type,
-                )
-                item["graph"] = test_data
-            else:
-                raise ValueError(f"Invalid split group: {self.split_group}")
-            # self.dataset.data, self.dataset.slices = self.dataset.collate(
-            #    [train_data, valid_data, test_data]
-            # )
-        elif cls == "WN18RR":
-            # dataset = WordNet18RR(**cfg.dataset)
-            # convert wn18rr into the same format as fb15k-237
-            data = self.dataset.data
-            num_nodes = int(data.edge_index.max()) + 1
-            num_relations = int(data.edge_type.max()) + 1
-            edge_index = data.edge_index[:, data.train_mask]
-            edge_type = data.edge_type[data.train_mask]
-            edge_index = torch.cat([edge_index, edge_index.flip(0)], dim=-1)
-            edge_type = torch.cat([edge_type, edge_type + num_relations])
-            if self.split_group == "train":
-                train_data = Data(
-                    edge_index=edge_index,
-                    edge_type=edge_type,
-                    num_nodes=num_nodes,
-                    target_edge_index=data.edge_index[:, data.train_mask],
-                    target_edge_type=data.edge_type[data.train_mask],
-                )
-                item["graph"] = train_data
-            elif self.split_group == "dev":
-                valid_data = Data(
-                    edge_index=edge_index,
-                    edge_type=edge_type,
-                    num_nodes=num_nodes,
-                    target_edge_index=data.edge_index[:, data.val_mask],
-                    target_edge_type=data.edge_type[data.val_mask],
-                )
-                item["graph"] = valid_data
-            elif self.split_group == "test":
-                test_data = Data(
-                    edge_index=edge_index,
-                    edge_type=edge_type,
-                    num_nodes=num_nodes,
-                    target_edge_index=data.edge_index[:, data.test_mask],
-                    target_edge_type=data.edge_type[data.test_mask],
-                )
-                item["graph"] = test_data
-            else:
-                raise ValueError(f"Invalid split group: {self.split_group}")
-            # self.dataset.data, self.dataset.slices = self.dataset.collate(
-            #     [train_data, valid_data, test_data]
-            # )
-            # train_data, valid_data, test_data = dataset[0], dataset[1], dataset[2] # from 'run.py'
-            self.dataset.num_relations = num_relations * 2
-
-        else:
-            raise ValueError("Unknown dataset `%s`" % cls)
+        item["graph"] = self.data
+        return self.dataset[0]
 
     def assign_splits(self, metadata_json) -> None:
         """
@@ -576,22 +441,3 @@ class IndRelLinkPredDataset(AbstractDataset):
             default="default_image_loader",
             help="input loader",
         )
-
-
-def to_list(value: Any) -> Sequence:
-    if isinstance(value, Sequence) and not isinstance(value, str):
-        return value
-    else:
-        return [value]
-
-
-def files_exist(files: List[str]) -> bool:
-    # NOTE: We return `False` in case `files` is empty, leading to a
-    # re-processing of files on every instantiation.
-    return len(files) != 0 and all([os.path.exists(f) for f in files])
-
-
-def _repr(obj: Any) -> str:
-    if obj is None:
-        return "None"
-    return re.sub("(<.*?)\\s.*(>)", r"\1\2", obj.__repr__())
