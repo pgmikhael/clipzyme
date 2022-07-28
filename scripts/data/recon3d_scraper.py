a05842ace98c1ba2c745cdf05f8f660465fa5eae
Itamar Chinn <itamarc@mit.edu>
2022-07-28 09:22:20 -0500

    added bigg scraper metabolites to recon3d

--- scripts/data/recon3d_scraper.py
+++ scripts/data/recon3d_scraper.py
@@ -5,7 +5,8 @@ from cobra.core.reaction import Reaction
 import pandas as pd
 from collections import defaultdict
 from bioservices import UniProt
-from bioservices.apps.fasta import FASTA 
+from bioservices.apps.fasta import FASTA
+from scripts.data.bigg_scraper import link_metabolite_to_db
 from p_tqdm import p_map
 import requests
 import rdkit.Chem as Chem
@@ -14,7 +15,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# https://www.nature.com/articles/nbt.4072 
+# https://www.nature.com/articles/nbt.4072
 RECON3_METABOLITES = pd.read_excel(
     "/Mounts/rbg-storage1/datasets/Metabo/VMH/Recon3D/41587_2018_BFnbt4072_MOESM11_ESM.xlsx",
     sheet_name="Supplementary Data File 14",
@@ -28,7 +29,9 @@ RECON3_PROTEINS = pd.read_excel(
 RECON3_PROTEINS.fillna("", inplace=True)
 
 # https://github.com/SBRG/ssbio
-RECON3_PROTEINS_GEMPRO = pd.read_csv("/Mounts/rbg-storage1/datasets/Metabo/Recon3D/Recon3D_GP/data/DF_GEMPRO.csv")
+RECON3_PROTEINS_GEMPRO = pd.read_csv(
+    "/Mounts/rbg-storage1/datasets/Metabo/Recon3D/Recon3D_GP/data/DF_GEMPRO.csv"
+)
 RECON3_PROTEINS_GEMPRO.fillna("", inplace=True)
 
 RECON3_DATASET_PATH = (
@@ -76,7 +79,14 @@ def get_metabolite_metadata(metabolite: Metabolite) -> dict:
                 ).json()
                 meta_dict["smiles"] = vmh_page["results"][0]["smile"]
             except:
-                meta_dict["smiles"] = None
+                bigg_scraped_metadata = link_metabolite_to_db(metabolite)
+                if "smiles" in bigg_scraped_metadata and (
+                    len(bigg_scraped_metadata["smiles"]) > 0
+                    or bigg_scraped_metadata["smiles"] is not None
+                ):
+                    meta_dict.update(bigg_scraped_metadata)
+                else:
+                    meta_dict["smiles"] = None
 
     return meta_dict
 
@@ -119,7 +129,9 @@ def get_reaction_elements(rxn: Reaction) -> dict:
         rxn_dict["products"].append(metabolite_dict)
 
     # 3. Get proteins
-    protein_meta = RECON3_PROTEINS_GEMPRO[RECON3_PROTEINS_GEMPRO["m_reaction"] == rxn.id]
+    protein_meta = RECON3_PROTEINS_GEMPRO[
+        RECON3_PROTEINS_GEMPRO["m_reaction"] == rxn.id
+    ]
     proteins = []
     for i, row in protein_meta.iterrows():
         if row["m_gene"] == "":
@@ -127,28 +139,30 @@ def get_reaction_elements(rxn: Reaction) -> dict:
             continue
 
         protein_dict = {
-                "uniprot": populate_empty_with_none(row["seq_uniprot"]),
-                "bigg_gene_id": populate_empty_with_none(row["m_gene"]),
-                "entrez": populate_empty_with_none(row["m_gene"]),
-                "is_experimental": populate_empty_with_none(row["struct_is_experimental"]),
-                "pdb": populate_empty_with_none(row["struct_pdb"]),
+            "uniprot": populate_empty_with_none(row["seq_uniprot"]),
+            "bigg_gene_id": populate_empty_with_none(row["m_gene"]),
+            "entrez": populate_empty_with_none(row["m_gene"]),
+            "is_experimental": populate_empty_with_none(row["struct_is_experimental"]),
+            "pdb": populate_empty_with_none(row["struct_pdb"]),
         }
         # check if existing
         fasta_path = f"Recon3D_GP/genes/{row['m_gene']}/{row['m_gene']}_protein/sequences/{row['seq_file']}"
         if os.path.exists(fasta_path):
             fasta_service = FASTA()
-            fasta_service.read_fasta(fasta_path) 
-            protein_dict["protein_sequence"] = fasta_service.sequence,
-        
-        else:   
+            fasta_service.read_fasta(fasta_path)
+            protein_dict["protein_sequence"] = (fasta_service.sequence,)
+
+        else:
             if protein_dict["uniprot"]:
                 try:
-                    protein_dict["protein_sequence"] = uniprot_service.get_fasta_sequence(
-                        protein_dict["uniprot"]
-                    )
+                    protein_dict[
+                        "protein_sequence"
+                    ] = uniprot_service.get_fasta_sequence(protein_dict["uniprot"])
                 except:
                     try:
-                        protein_dict["protein_sequence"] = uniprot_service.get_fasta_sequence(
+                        protein_dict[
+                            "protein_sequence"
+                        ] = uniprot_service.get_fasta_sequence(
                             protein_dict["uniprot"].split("-")[0]
                         )
                     except:
