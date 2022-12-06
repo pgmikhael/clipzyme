d34067df34832f2521fd33b4adc099c7aef9c302
Peter G. Mikhael <pgmikhael@rbgquanta1.csail.mit.edu>
2022-12-06 17:19:19 -0500

    seq2seq generation

--- nox/models/seq2seq.py
+++ nox/models/seq2seq.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import copy
+import inspect
 from typing import Union, Tuple, Any, List, Dict, Optional
 from nox.utils.classes import set_nox_type
 from nox.models.abstract import AbstractModel
@@ -23,6 +24,20 @@ class ReactionEncoder(AbstractModel):
         self.config = self.model.config
         self.args = args
         self.register_buffer("devicevar", torch.zeros(1, dtype=torch.int8))
+
+        self.generation_config = self.make_generation_config(args)
+    
+    def make_generation_config(self, args):
+        generate_args = inspect.signature(self.model).parameters
+        args_dict = vars(args)
+        gen_config = {}
+        
+        for k in generate_args:
+            if f"generation_{k}" in args_dict:
+                gen_config[k] = args_dict[f"generation_{k}"]
+
+        return gen_config
+        
 
     def get_transformer_model(self, args):
         if args.transformer_model == "bert":
@@ -149,23 +164,57 @@ class ReactionEncoder(AbstractModel):
             labels.view(-1),
         )
 
-        metric_labels = labels.view(-1)
-        metric_logits = shifted_logits.reshape(-1, self.model.decoder.config.vocab_size)[metric_labels!= -100]
-        metric_labels = metric_labels[ metric_labels!= -100]
+        metric_labels = labels#.view(-1)
+        metric_logits = shifted_logits #.reshape(-1, self.model.decoder.config.vocab_size)[metric_labels!= -100]
+        metric_labels = metric_labels  #[ metric_labels!= -100]
         output = {
             "loss": loss,
             "logit": metric_logits,
             "y": metric_labels,
-            "decoder_hidden_states": decoder_outputs.hidden_states,
-            "past_key_values": decoder_outputs.past_key_values,
-            "decoder_hidden_states": decoder_outputs.hidden_states,
-            "decoder_attentions": decoder_outputs.attentions,
-            "cross_attentions": decoder_outputs.cross_attentions,
-            "encoder_last_hidden_state": encoder_outputs.last_hidden_state,
-            "encoder_hidden_states": encoder_outputs.hidden_states,
-            "encoder_attentions": encoder_outputs.attentions,
+            #"decoder_hidden_states": decoder_outputs.hidden_states,
+            #"past_key_values": decoder_outputs.past_key_values,
+            #"decoder_hidden_states": decoder_outputs.hidden_states,
+            #"decoder_attentions": decoder_outputs.attentions,
+            #"cross_attentions": decoder_outputs.cross_attentions,
+            #"encoder_last_hidden_state": encoder_outputs.last_hidden_state,
+            #"encoder_hidden_states": encoder_outputs.hidden_states,
+            #"encoder_attentions": encoder_outputs.attentions,
         }
         return output
+
+    def generate(self, batch, **kwargs):
+        """Applies auto-regressive generation for reaction prediction
+        Usage: https://huggingface.co/docs/transformers/main/en/main_classes/text_generation#transformers.GenerationMixin
+
+        Args:
+            batch (dict): dictionary with input reactants
+            decoder_start_token_id = 2,
+            bos_token_id = 2,
+            max_new_tokens=100
+        """
+        bos_id = self.tokenizer.cls_token_id
+        eos_id = self.tokenizer.eos_token_id
+        pad_id = self.tokenizer.pad_token_id
+
+        encoder_input_ids = self.tokenize(batch["reactants"], self.tokenizer, self.args)
+        generated_ids = self.model.generate(
+            input_ids=encoder_input_ids,
+            decoder_start_token_id = bos_id,
+            bos_token_id = bos_id,
+            eos_token_id = eos_id,
+            pad_token_id = pad_id,
+            attention_mask = encoder_input_ids['attention_mask'],
+            output_scores=True,
+            return_dict_in_generate=True,
+            early_stopping=True,
+            **kwargs
+        )
+        generated_samples = self.tokenizer.batch_decode(generated_ids)
+        processed_samples = []
+        for sample in generated_samples:
+            processed_samples.append( sample[:sample.index(tokenizer.eos_token)] )
+        
+        return processed_samples
 
     @staticmethod
     def add_args(parser) -> None:
@@ -262,7 +311,48 @@ class ReactionEncoder(AbstractModel):
             default=None,
             help="path to saved model if loading from pretrained",
         )
-
+        parser.add_argument(
+            "--generation_max_new_tokens",
+            type=int,
+            default=200,
+            help="The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt.",
+        )
+        parser.add_argument(
+            "--generation_num_beams",
+            type=int,
+            default=1,
+            help="Number of beams for beam search. 1 means no beam search.",
+        )
+        parser.add_argument(
+            "--generation_num_return_sequences",
+            type=int,
+            default=1,
+            help="The number of independently computed returned sequences for each element in the batch. <= num_beams",
+        )
+        parser.add_argument(
+            "--generation_num_beam_groups",
+            type=int,
+            default=1,
+            help="Number of groups to divide `num_beams` into in order to ensure diversity among different groups of beams. [this paper](https://arxiv.org/pdf/1610.02424.pdf) for more details"
+        )
+        parser.add_argument(
+            "--generation_do_sample",
+            action='store_true',
+            default=False,
+            help="Whether or not to use sampling ; use greedy decoding otherwise."
+        )
+        parser.add_argument(
+            "--generation_early_stopping",
+            action='store_true',
+            default=False,
+            help="Whether to stop the beam search when at least `num_beams` sentences are finished per batch or not"
+        )
+        parser.add_argument(
+            "--generation_renormalize_logits",
+            action='store_true',
+            default=False,
+            help=" Whether to renormalize the logits after applying all the logits processors or warpers (including the custom ones). It's highly recommended to set this flag to `True` as the search algorithms suppose the score logits are normalized but some logit processors or warpers break the normalization."
+        )
 
 @register_object("enzymatic_reaction_encoder", "model")
 class EnzymaticReactionEncoder(ReactionEncoder):
