58fc7da3fc89002a7042ceaae92bca8579329f83
Peter G. Mikhael <pgmikhael@rosetta18.csail.mit.edu>
2022-12-21 20:21:37 -0500

    add reaction mapper

--- nox/models/enzymenet.py
+++ nox/models/enzymenet.py
@@ -75,15 +75,27 @@ class EnzymeActiveSiteModel(AbstractModel):
         super(EnzymeActiveSiteModel, self).__init__()
 
         self.args = args
+        classifier_args = copy.deepcopy(args)
         self.protein_encoder = get_object(args.protein_encoder_name, "model")(args)
-        #self.reaction_encoder = get_object(args.reaction_encoder_name, "model")(args)
-        self.mlp = get_object(args.classifier_name, "model")(args)
+        if args.reaction_encoder_name is not None:
+            self.reaction_encoder = get_object(args.reaction_encoder_name, "model")(args)
+            classifier_args.mlp_input_dim = args.mlp_input_dim + args.hidden_size
+        self.mlp = get_object(args.classifier_name, "model")(classifier_args)
 
     def forward(self, batch=None):
         batch_size = len(batch["sequence"])
         seq_len = [len(p) for p in batch["sequence"]]
         sequence_dict = self.protein_encoder(batch["sequence"])
         hidden = sequence_dict["token_hiddens"][:, 1:-1]  # B, seq_len, hidden_dim
+        
+        
+        if hasattr(self, "reaction_encoder"):
+            rxn_dict = self.reaction_encoder({"x": batch["reaction"]})
+            rxn_h = rxn_dict["hidden"].unsqueeze(1) # B, 1, hidden_dim
+            rxn_h = torch.repeat_interleave(rxn_h, hidden.shape[1], dim = 1)
+            hidden = torch.concat([hidden, rxn_h], dim = -1)
+        
+        
         output = self.mlp({"x": hidden})  # B, seq_len, num_classes
  
         labels = batch["residue_mask"]
@@ -112,7 +124,7 @@ class EnzymeActiveSiteModel(AbstractModel):
             "--reaction_encoder_name",
             type=str,
             action=set_nox_type("model"),
-            default="reaction_mlm",
+            default=None,
             help="Name of encoder to use",
         )
         parser.add_argument(
