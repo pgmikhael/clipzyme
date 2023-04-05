02a500f52b8805816e2c49440ba0c895bf0cef65
Peter G. Mikhael <pgmikhael@rosettacpu2.csail.mit.edu>
2023-04-04 20:07:14 -0400

    update to iterate through uniprot links

--- scripts/data/ecreact_maker.py
+++ scripts/data/ecreact_maker.py
@@ -5,9 +5,15 @@ from p_tqdm import p_map
 import requests
 import pandas as pd
 import re, os
+import requests
+from requests.adapters import HTTPAdapter, Retry
+import re 
+import pickle
 
 UNIPROT_QUERY_URL = "https://rest.uniprot.org/uniprotkb/search?query=reviewed:true+AND+ec:{}&format=json&fields=id,sequence,cc_alternative_products&size=500"
 UNIPROT_ENTRY_URL = "https://rest.uniprot.org/uniprotkb/{}.fasta"
+RE_NEXT_LINK = re.compile(r'<(.+)>; rel="next"')
+
 
 parser = argparse.ArgumentParser(
     description="Make EC React Dataset (https://github.com/rxn4chemistry/biocatalysis-model)"
@@ -30,7 +36,31 @@ parser.add_argument(
     default=False,
     help="if from IBM processed splits",
 )
+parser.add_argument(
+    "--from_random_splits",
+    action="store_true",
+    default=False,
+    help="if from random splits",
+)
+parser.add_argument(
+    "--get_ec_to_uniprots",
+    action="store_true",
+    default=False,
+    help="whether to get uniprot ids",
+)
+parser.add_argument(
+    "--add_isoforms",
+    action="store_true",
+    default=False,
+    help="whether to add isoforms",
+)
 
+
+def get_next_link(headers):
+    if "Link" in headers:
+        match = RE_NEXT_LINK.match(headers["Link"])
+        if match:
+            return match.group(1)
 
 def parse_fasta(f):
     """Parse fasta data
@@ -72,6 +102,41 @@ def transform_ec_number(ec_str):
     ec_digits = re.findall(r"\d+|-", ec_str)
     ec = ".".join(ec_digits)
     return ec
+
+
+def get_uniprots_from_ec(ec):
+    iso_dataset = []
+    uni2seq = {}
+    
+    # get the proteins sequences; while loop
+    batch_url=UNIPROT_QUERY_URL.format(ec)
+    while batch_url:
+        uniprot_results = requests.get(batch_url)
+        if uniprot_results.status_code == 200:
+            batch_url = get_next_link(uniprot_results.headers)
+            uniprot_results = uniprot_results.json()["results"]
+            num_uniprot_results = len(uniprot_results)
+            for uniprot_result in uniprot_results:
+                uniprot_id = uniprot_result["primaryAccession"]
+                sequence = uniprot_result["sequence"]["value"]
+                uni2seq[uniprot_id] = sequence
+
+                for comment in uniprot_result.get("comments", []):
+                    for isoform in comment.get("isoforms", []):
+                        isoform_uniprots = isoform["isoformIds"]
+                        for isoform_u in isoform_uniprots:
+                            iso_dataset.append(isoform_u)
+            
+            if batch_url is None:
+                batch_url = False
+
+        else:
+            batch_url=False
+    
+    iso_dataset = list(set(iso_dataset))
+    iso_dataset = [u for u in iso_dataset if u not in uni2seq]
+
+    return (uni2seq, iso_dataset)
 
 
 if __name__ == "__main__":
@@ -127,71 +192,12 @@ if __name__ == "__main__":
             for tok in vocab:
                 f.write(tok)
                 f.write("\n")
+        
+        json.dump(dataset, open(args.output_file_path, "w"))
 
-    else:
-
+    elif args.from_random_splits:
         react_dataset = pd.read_csv(args.react_dir_or_path)
-        # for i, row in tqdm(react_dataset.iterrows(), total=len(react_dataset)):
-        def parse_react_row(row):
-            dataset = []
-            iso_dataset = []
-            uni2seq = {}
-            rxn_smiles = row["rxn_smiles"]
-            ec = row["ec"]
-            db_source = row["source"]
-
-            full_reactants, products = rxn_smiles.split(">>")
-            products = products.split(".")
-            reactants_str, ec_str = full_reactants.split("|")
-            reactants = reactants_str.split(".")
-            # get the proteins sequences; while loop
-            num_uniprot_results = 500
-            while num_uniprot_results >= 500:
-                uniprot_results = requests.get(UNIPROT_QUERY_URL.format(ec))
-                if uniprot_results.status_code == 200:
-                    uniprot_results = uniprot_results.json()["results"]
-                    num_uniprot_results = len(uniprot_results)
-                    for uniprot_result in uniprot_results:
-                        uniprot_id = uniprot_result["primaryAccession"]
-                        sequence = uniprot_result["sequence"]["value"]
-                        uni2seq[uniprot_id] = sequence
-
-                        for comment in uniprot_result["comments"]:
-                            for isoform in comment.get("isoforms", []):
-                                isoform_uniprots = isoform["isoformIds"]
-                                for isoform_u in isoform_uniprots:
-                                    iso_dataset.append(isoform_u)
-
-                else:
-                    num_uniprot_results = 0
-
-            for uni, seq in uni2seq.items():
-                dataset.append(
-                    {
-                        "reactants": reactants,
-                        "products": products,
-                        "sequence": seq,
-                        "ec": ec,
-                        "uniprot_id": uni,
-                        "db_source": db_source,
-                    }
-                )
-
-            iso_dataset = list(set(iso_dataset))
-            iso_dataset = [u for u in iso_dataset if u not in uni2seq]
-            iso_dataset = [
-                {
-                    "reactants": reactants,
-                    "products": products,
-                    "ec": ec,
-                    "uniprot_id": uni,
-                    "db_source": db_source,
-                }
-                for uni in iso_dataset
-            ]
-
-            return (dataset, iso_dataset)
-
+    
         # transform csv to json
         react_dataset_rows = react_dataset.to_dict("records")
         reactions_dataset = []
@@ -217,18 +223,29 @@ if __name__ == "__main__":
             open(args.react_dir_or_path.replace(".csv", ".json"), "w"),
         )
 
+    if args.get_ec_to_uniprots:
+        react_dataset_rows = json.load(open(args.react_dir_or_path, "r"))
+        ecs = list(set(r['ec'] for r in react_dataset_rows))
+        
         # match ec to uniprots, sequences, and isoforms
-        reference_dataset = p_map(parse_react_row, react_dataset_rows)
-        dataset, iso_dataset = [], []
-        for d in reference_dataset:
-            dataset.extend(d[0])
-            iso_dataset.extend(d[1])
+        uniprot_results = p_map(get_uniprots_from_ec, ecs)
 
-        # pass through isoforms
-        isoform_uniprots = [d["uniprot_id"] for d in iso_dataset]
-        isform_sequences = p_map(get_protein_fasta, isoform_uniprots)
-        for i, sample in enumerate(iso_dataset):
-            sample["sequence"] = isform_sequences[i]
-            dataset.append(sample)
+        ec2uniprot = {}         # ec to canonical uniprot
+        ec2iso_uniprot = {}     # ec to isoform uniprot
+        uni2seq = {}            # uniprot to sequence
+        iso_uniprots = set()    # set of isoform ids 
+        for ec, uresult in zip(ecs, uniprot_results):
+            ec2uniprot[ec] = list(uresult[0].keys())
+            ec2iso_uniprot[ec] = uresult[1]
+            iso_uniprots.update(uresult[1])
+            uni2seq.update(uresult[0])
 
-    json.dump(dataset, open(args.output_file_path, "w"))
+        if args.add_isoforms:
+            # pass through isoforms
+            isoform_sequences = p_map(get_protein_fasta, iso_uniprots)
+            isoform2sequence = {i:s for i,s in zip(iso_uniprots, isoform_sequences)}
+            uni2seq.update(isoform2sequence)
+
+
+    pickle.dump(ec2uniprot, open(f"{args.output_file_path}/ec2uniprot.p", "wb"))
+    pickle.dump(uni2seq, open(f"{args.output_file_path}/uniprot2sequence.p", "wb"))
