f9c6fbdd5861b6eee05f66ab18469e20f015742c
Peter G. Mikhael <pgmikhael@rosetta16.csail.mit.edu>
2023-04-11 19:16:07 -0400

    topk for set of mols

--- nox/learning/metrics/reactions.py
+++ nox/learning/metrics/reactions.py
@@ -83,3 +83,79 @@ class TopK(Metric, Nox):
             default=[1],
             help="Values of k for which to obtain top-k accuracies",
         )
+
+@register_object("mol_topk", "metric")
+class MolTopK(Metric, Nox):
+    def __init__(self, args):
+        super().__init__()
+        higher_is_better: Optional[bool] = True
+        is_differentiable = False
+        full_state_update: bool = False
+
+        self.topk_values = args.mol_topk_values
+        self.add_state("percent_coverage", default=torch.tensor(0.0), dist_reduce_fx="sum")
+    
+        for k in args.mol_topk_values:
+            self.add_state(
+                f"num_correct_top{k}", default=torch.tensor(0), dist_reduce_fx="sum"
+            )
+            self.add_state(
+                f"total_top{k}", default=torch.tensor(0), dist_reduce_fx="sum"
+            )
+
+    @property
+    def metric_keys(self):
+        return ["pred_smiles", "all_smiles"]
+
+    def update(self, predictions_dict, args) -> None:
+        """Computes top-k accuracy for some defined value(s) of k"""
+        preds = predictions_dict["pred_smiles"]  # B, k (list)
+        golds = predictions_dict["all_smiles"]  # B, k (list)
+        golds = [[canonicalize_smiles(g) for g in glist] for glist in golds]
+        ranks = []
+        for top_preds, gold in zip(preds, golds):
+            standardized_preds = [canonicalize_smiles(g) for g in top_preds]
+            matches = [p in gold for p in standardized_preds]    
+            match_idx = matches.index(True) + 1 if sum(matches) > 0 else 0
+            ranks.append(match_idx)
+
+            self.percent_coverage += len(set(gold).intersection(set(top_preds))) / max(len(gold), len(set(top_preds)))
+
+
+        ranks_np = np.array(ranks)
+
+        for k in self.topk_values:
+            num_samples = len(golds)
+            num_correct = np.logical_and(ranks_np > 0, ranks_np <= k).sum()
+            last_correct = getattr(self, f"num_correct_top{k}")
+            last_total = getattr(self, f"total_top{k}")
+
+            setattr(
+                self, f"num_correct_top{k}", last_correct + torch.tensor(num_correct)
+            )
+            setattr(self, f"total_top{k}", last_total + torch.tensor(num_samples))
+
+    def compute(self) -> Dict:
+        stats_dict = {
+            f"mol_top_{k}": getattr(self, f"num_correct_top{k}")
+            / getattr(self, f"total_top{k}")
+            for k in self.topk_values
+        }
+        stats_dict["mol_percent_coverage"] = self.percent_coverage / getattr(self, f"total_top{self.topk_values[0]}")
+
+        return stats_dict
+
+    @staticmethod
+    def add_args(parser) -> None:
+        """Add class specific args
+
+        Args:
+            parser (argparse.ArgumentParser): argument parser
+        """
+        parser.add_argument(
+            "--mol_topk_values",
+            type=int,
+            nargs="+",
+            default=[1],
+            help="Values of k for which to obtain top-k accuracies",
+        )
