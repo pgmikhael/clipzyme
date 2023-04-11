df35c858085ef0fc82591865cc5fbe34912392a8
Itamar Chinn <itamar@chinn.com>
2023-04-11 16:24:41 -0400

    removed missing import

--- nox/__init__.py
+++ nox/__init__.py
@@ -88,7 +88,6 @@ import nox.models.seq2seq
 import nox.models.contrastive
 import nox.models.graph_denoisers
 import nox.models.digress
-import nox.models.clip
 import nox.models.esm_decoder
 import nox.models.protmol
 import nox.models.noncanonnet
