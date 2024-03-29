c3b097a1bc7e3a31ea9fc7e71b7c34a591d0c92a
Peter Mikhael <pgmikhael@gmail.com>
2024-03-29 16:19:18 -0400

    util to collect saved data

--- clipzyme/utils/screening.py
+++ clipzyme/utils/screening.py
@@ -1,59 +1,107 @@
+import json, os
+from typing import Union, NamedTuple
+from pathlib import Path
 import torch
 import pickle
 from p_tqdm import p_map
 from functools import partial
+import pandas as pd
 
 
-def collect_screening_results(config):
-    def read_protein(u, path):
-        try:
-            d = pickle.load(
-                open(
-                    f"/data/rsg/mammogram/pgmikhael/clip_hiddens/{path}/sample_{u}.hiddens",
-                    "rb",
-                )
-            )
-            return d["hidden"]
-        except:
-            return
+class ScreeningResults(NamedTuple):
+    predictions: pd.DataFrame
+    protein_hiddens: torch.Tensor
+    reaction_hiddens: torch.Tensor
+    protein_ids: list
+    reaction_ids: list
 
-    screening_set = pickle.load(
-        open(
-            "/Mounts/rbg-storage1/datasets/Enzymes/uniprot2sequence_standard_set_structs.p",
-            "rb",
-        )
+
+def load_hidden(hiddens_dir: Path, file: str) -> Union[torch.Tensor, None]:
+    """
+    Load hidden representation from a file
+
+    Parameters
+    ----------
+    hiddens_dir : Path
+        directory where hidden representations are saved
+    file : str
+        file name of hidden representation
+
+    Returns
+    -------
+    Union[torch.Tensor, None]
+        hidden representation tensor or None if file does not exist
+    """
+    try:
+        return torch.load(hiddens_dir.joinpath(file))
+    except FileNotFoundError:
+        return
+
+
+def collect_screening_results(config_path: str) -> ScreeningResults:
+    """
+    Collect screening results from a screening experiment
+
+    Parameters
+    ----------
+    config_path : str
+        path to screening experiment configuration file
+
+    Returns
+    -------
+    ScreeningResults
+        screening results, including predictions, protein hiddens, reaction hiddens, protein ids, and reaction ids
+    """
+    config = json.load(open(config_path, "r"))
+    inference_dir = config["inference_dir"]
+    save_hiddens = config["save_hiddens"]
+    save_predictions = config["save_predictions"]
+    checkpoint_path = Path(config["checkpoint_path"])
+    dataset_path = Path(config["dataset_file_path"])
+    # get directory where hiddens are saved
+    hiddens_dir = os.path.join(inference_dir, checkpoint_path.stem, dataset_path.stem)
+
+    predictions = None
+    if save_predictions:
+        # get path to predictions (screening scores)
+        predictions_path = os.path.join(hiddens_dir, "predictions.csv")
+        predictions = pd.read_csv(predictions_path)
+
+    hiddens_dir = Path(hiddens_dir)
+    assert hiddens_dir.is_dir(), f"{hiddens_dir} is not a directory"
+
+    protein_hiddens, reaction_hiddens = None, None
+    protein_ids, reaction_ids = None, None
+    if save_hiddens:
+        load_hidden_func = partial(load_hidden, path=hiddens_dir)
+        protein_files = list(hiddens_dir.glob("sample_*.protein.pt"))
+        reaction_files = list(hiddens_dir.glob("sample_*.reaction.pt"))
+
+        load_hidden_func = partial(load_hidden, path=hiddens_dir)
+        protein_hiddens = p_map(load_hidden_func, protein_files)
+        reaction_hiddens = p_map(load_hidden_func, reaction_files)
+
+        protein_ids = [f.stem.split("_")[-1] for f in protein_files]
+        reaction_ids = [f.stem.split("_")[-1] for f in reaction_files]
+
+        protein_ids = [u for u, h in zip(protein_ids, protein_hiddens) if h is not None]
+        reaction_ids = [
+            u for u, h in zip(reaction_ids, reaction_hiddens) if h is not None
+        ]
+
+        protein_hiddens = [h for h in protein_hiddens if h is not None]
+        reaction_hiddens = [h for h in reaction_hiddens if h is not None]
+
+        protein_hiddens = torch.stack(protein_hiddens)
+        reaction_hiddens = torch.stack(reaction_hiddens)
+
+    # collect
+    output = ScreeningResults(
+        predictions=predictions,
+        protein_hiddens=protein_hiddens,
+        reaction_hiddens=reaction_hiddens,
+        protein_ids=protein_ids,
+        reaction_ids=reaction_ids,
     )
-    screening_set = {k: v for k, v in screening_set.items() if v != ""}
-    # has structure and msa
-    alphafold_files = pickle.load(
-        open("/Mounts/rbg-storage1/datasets/Metabo/alphafold_enzymes.p", "rb")
-    )
-    msa_files = pickle.load(
-        open("/Mounts/rbg-storage1/datasets/Enzymes/uniprot2msa_embedding.p", "rb")
-    )
-    screening_set = {
-        k: v
-        for k, v in screening_set.items()
-        if (k in alphafold_files) and (k in msa_files) and (len(v) <= 650)
-    }
-    len(screening_set)
-    screening_set_uniprots = list(screening_set.keys())
 
-    experiment_name = "d8faada03e032e26e2216ea83a88529cepoch=22"
-
-    read_protein_func = partial(read_protein, path=experiment_name)
-    hiddens = p_map(read_protein_func, screening_set_uniprots)
-
-    all_ec_uniprots_ = [
-        u for u, h in zip(screening_set_uniprots, hiddens) if h is not None
-    ]
-    hiddens = [h for h in hiddens if h is not None]
-    hiddens = torch.stack(hiddens)
-
-    pickle.dump(
-        {"hiddens": hiddens, "uniprots": all_ec_uniprots_},
-        open(
-            f"/data/rsg/mammogram/pgmikhael/notebooks/EScreening/precomputed_{experiment_name}.p",
-            "wb",
-        ),
-    )
+    return output
